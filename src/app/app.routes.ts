bfdce78e837e69460dfddc185e6b7bef28f41214
Rekurt <sabrlitet@gmail.com>
2026-02-26 11:39:57 +0300

    gebering components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from '../layouts/main-layout/main-layout';
+import { AdminLayout } from '../layouts/admin-layout/admin-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+  path: '',
+  component: MainLayout,
+  children: [
+    // { path: '', component: HomeComponent },
+    // { path: 'products', component: ProductsComponent },
+    // { path: 'cart', component: CartComponent },
+  ]
+},
+{
+  path: 'admin',
+  component: AdminLayout,
+  children: [
+    // { path: '', component: AdminDashboardComponent },
+    // { path: 'products', component: AdminProductsComponent },
+  ]
+}
+
+];
