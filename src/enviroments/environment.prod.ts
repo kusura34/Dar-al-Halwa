d5bb230838c96ab1ac9d039f178b001380bf5a8d
Rekurt <sabrlitet@gmail.com>
2026-03-01 14:30:55 +0300

    build

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD2xkQgQSZ7vX9K8itbwtoaUEx3neeS3uA",
+    authDomain: "dar-al-halwa-925db.firebaseapp.com",
+    projectId: "dar-al-halwa-925db",
+    storageBucket: "dar-al-halwa-925db.firebasestorage.app",
+    messagingSenderId: "151841589100",
+    appId: "1:151841589100:web:6ebdf229a53ac7cc11d549",
+    measurementId: "G-YR6EJ8V52K"
+  }
+};
