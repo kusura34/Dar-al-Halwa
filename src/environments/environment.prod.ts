9eef4a27a157908bbfd89838448568523001a61d
Rekurt-116 <sabrlitet@gmail.com>
2026-05-31 15:52:03 +0300

    fix deploy for firestore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+
+    firebase: {
+    apiKey: 'AIzaSyD2xkQgQSZ7vX9K8itbwtoaUEx3neeS3uA',
+
+    authDomain: 'dar-al-halwa-925db.firebaseapp.com',
+
+    databaseURL: 'https://dar-al-halwa-925db-default-rtdb.europe-west1.firebasedatabase.app',
+
+    projectId: 'dar-al-halwa-925db',
+
+    storageBucket: 'dar-al-halwa-925db.firebasestorage.app',
+
+    messagingSenderId: '151841589100',
+
+    appId: '1:151841589100:web:6ebdf229a53ac7cc11d549',
+
+    measurementId: 'G-YR6EJ8V52K',
+  },
+}
